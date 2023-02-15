937c378776df29a0dcf8b086f52b4baec4cb086c
venecia <kakushavenecia@gmail.com>
2023-02-15 23:34:43 +0300

    Changed the components folder and naming of the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './appcomponents/login-component/login-component.component';
-import { SigninComponentComponent } from './appcomponents/signin-component/signin-component.component';
-import { CategoriesComponentComponent } from './appcomponents/categories-component/categories-component.component';
-import { CheckoutComponentComponent } from './appcomponents/checkout-component/checkout-component.component';
-import { HomepageComponentComponent } from './appcomponents/homepage-component/homepage-component.component';
-import { ProductsComponentComponent } from './appcomponents/products-component/products-component.component';
-import { CartComponentComponent } from './appcomponents/cart-component/cart-component.component';
-import { ProfileComponentComponent } from './appcomponents/profile-component/profile-component.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent,
-    SigninComponentComponent,
-    CategoriesComponentComponent,
-    CheckoutComponentComponent,
-    HomepageComponentComponent,
-    ProductsComponentComponent,
-    CartComponentComponent,
-    ProfileComponentComponent
+    ProfileComponent,
+    CategoryListComponent,
+    SigninComponent,
+    LoginComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
