1ab81de64013f0097a0e1143559de26beeec3731
venecia <kakushavenecia@gmail.com>
2023-02-15 22:12:37 +0300

    docs-Initial setup of angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './appcomponents/login-component/login-component.component';
+import { SigninComponentComponent } from './appcomponents/signin-component/signin-component.component';
+import { CategoriesComponentComponent } from './appcomponents/categories-component/categories-component.component';
+import { CheckoutComponentComponent } from './appcomponents/checkout-component/checkout-component.component';
+import { HomepageComponentComponent } from './appcomponents/homepage-component/homepage-component.component';
+import { ProductsComponentComponent } from './appcomponents/products-component/products-component.component';
+import { CartComponentComponent } from './appcomponents/cart-component/cart-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    SigninComponentComponent,
+    CategoriesComponentComponent,
+    CheckoutComponentComponent,
+    HomepageComponentComponent,
+    ProductsComponentComponent,
+    CartComponentComponent
   ],
   imports: [
     BrowserModule,
